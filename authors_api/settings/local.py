c2fd1c1b22ff7e7478e223eaa699c514859f6d1b
Benjamin Idewor <benjaminidewor@gmail.com>
2022-07-15 08:58:00 +0100

    add celer and redis config

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -6,3 +6,10 @@ DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-r^+vl2y4^^&0aom=p88%j))4m8lu4z54h6fy^i5)9-pt9fis(-')
 
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
